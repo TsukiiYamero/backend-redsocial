fc942b5552d1e2bc87a81eb22af689ea18577c7c
SaiKisho <diegosferas@gmail.com>
2024-06-11 22:10:28 -0500

    ⭐ Primeros pasos

---
+++ connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connection = async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/db_redsocial")
+        console.log('Base de datos conectada');
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error al iniciar la base de datos");
+    }
+}
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 /* importar */
-
+const connection = require("./connection");
+const express = require("express");
+const cors = require("cors");
 /* conexion */
+connection();
 
 /* config cors */
+
+const app = require()
+const port = 3900;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /* conversion de datos */
 
 /* rutas */
-
+app.get('/test', (req, res) => {
+    return res.status(200).json({ message: 'hola mundo', id: '1' })
+})
 
 /* sevidor http */
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+})
